a0508bd234e8cba9ee3657b5b697e198f2a575e7
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-27 23:58:34 +0530

    Update in like routes and data form

--- routes/likeRoutes.js
+++ routes/likeRoutes.js
@@ -1,10 +1,33 @@
 import express from "express";
-import { toggleLike, getMyLikes } from "../controllers/likeControllers.js";
-import { protect } from "../middleware/authMiddleware.js";
+import {
+    toggleLike,
+    getMyLikes,
+} from "../controllers/likeController.js";
+
+import {
+    protect,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/mine", protect, getMyLikes);
-router.post("/post/:postId", protect, toggleLike);
+/*
+|--------------------------------------------------------------------------
+| Like Routes
+|--------------------------------------------------------------------------
+*/
+
+// Get all posts liked by the logged-in user.
+router.get(
+    "/mine",
+    protect,
+    getMyLikes
+);
+
+// Toggle like/unlike for a post.
+router.post(
+    "/:postId",
+    protect,
+    toggleLike
+);
 
 export default router;
