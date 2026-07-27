b66b550cd3bcfc76ea1d281203c8218c3036d1f0
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-27 23:55:17 +0530

    Update in comment models and data form

--- models/comment.js
+++ models/comment.js
@@ -2,14 +2,76 @@ import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema(
     {
-        post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
-        author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        content: { type: String, required: true, trim: true, maxlength: 1000 },
+        post: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            required: true,
+        },
+
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        parentComment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+            default: null,
+        },
+
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            maxlength: 1000,
+        },
+
+        isEdited: {
+            type: Boolean,
+            default: false,
+        },
+
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+
+        status: {
+            type: String,
+            enum: ["visible", "hidden"],
+            default: "visible",
+        },
+
+        likesCount: {
+            type: Number,
+            default: 0,
+        },
+
+        reportsCount: {
+            type: Number,
+            default: 0,
+        },
     },
-    { timestamps: true }
+    {
+        timestamps: true,
+    }
 );
 
-commentSchema.index({ post: 1, createdAt: -1 });
+commentSchema.index({
+    post: 1,
+    createdAt: -1,
+});
+
+commentSchema.index({
+    author: 1,
+});
+
+commentSchema.index({
+    parentComment: 1,
+});
 
 const Comment = mongoose.model("Comment", commentSchema);
+
 export default Comment;
