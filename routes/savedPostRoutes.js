e1398950e9b075de9a6b6b898d5153a99c8c9981
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-28 00:00:00 +0530

    Update in savepost routes and data form

--- routes/savedPostRoutes.js
+++ routes/savedPostRoutes.js
@@ -1,11 +1,41 @@
 import express from "express";
-import { toggleSavePost, getMySavedPosts, getMySavedPostIds } from "../controllers/savedPostControllers.js";
-import { protect } from "../middleware/authMiddleware.js";
+import {
+    toggleSavePost,
+    getMySavedPosts,
+    getMySavedPostIds,
+} from "../controllers/savedPostController.js";
+
+import {
+    protect,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/mine", protect, getMySavedPosts);
-router.get("/mine/ids", protect, getMySavedPostIds);
-router.post("/post/:postId", protect, toggleSavePost);
+/*
+|--------------------------------------------------------------------------
+| Saved Post Routes
+|--------------------------------------------------------------------------
+*/
+
+// Get all saved posts for the logged-in user.
+router.get(
+    "/mine",
+    protect,
+    getMySavedPosts
+);
+
+// Get IDs of all saved posts.
+router.get(
+    "/mine/ids",
+    protect,
+    getMySavedPostIds
+);
+
+// Save or unsave a post.
+router.post(
+    "/:postId",
+    protect,
+    toggleSavePost
+);
 
 export default router;
