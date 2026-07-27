5f4e8a6285fa1e936e886dcaeb4be965897ce767
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-27 23:56:38 +0530

    Update in savepost models and data form

--- models/savedPost.js
+++ models/savedPost.js
@@ -2,13 +2,42 @@ import mongoose from "mongoose";
 
 const savedPostSchema = new mongoose.Schema(
     {
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+        post: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            required: true,
+        },
     },
-    { timestamps: true }
+    {
+        timestamps: true,
+    }
 );
 
-savedPostSchema.index({ user: 1, post: 1 }, { unique: true });
+// Prevent duplicate saves.
+savedPostSchema.index(
+    {
+        user: 1,
+        post: 1,
+    },
+    {
+        unique: true,
+    }
+);
 
-const SavedPost = mongoose.model("SavedPost", savedPostSchema);
+// Optimize lookups by post.
+savedPostSchema.index({
+    post: 1,
+});
+
+const SavedPost = mongoose.model(
+    "SavedPost",
+    savedPostSchema
+);
+
 export default SavedPost;
