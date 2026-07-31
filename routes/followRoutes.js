3968e8a0733813b74258f96af3948133ebb02507
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-31 12:17:54 +0530

    Update follow route handlers and middleware protection

--- routes/followRoutes.js
+++ routes/followRoutes.js
@@ -6,16 +6,60 @@ import {
     getMyFollowing,
     getFollowers,
     getTopContributors,
-} from "../controllers/followControllers.js";
-import { protect } from "../middleware/authMiddleware.js";
+} from "../controllers/followController.js";
+
+import {
+    protect,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/following", protect, getMyFollowing);
-router.get("/top-contributors", protect, getTopContributors);
-router.get("/status/:userId", protect, getFollowStatus);
-router.get("/followers/:userId", protect, getFollowers);
-router.post("/:userId", protect, followUser);
-router.delete("/:userId", protect, unfollowUser);
+/*
+|--------------------------------------------------------------------------
+| Follow Routes
+|--------------------------------------------------------------------------
+*/
+
+// Get the logged-in user's following list.
+router.get(
+    "/following",
+    protect,
+    getMyFollowing
+);
+
+// Get top contributors ranked by followers.
+router.get(
+    "/top-contributors",
+    protect,
+    getTopContributors
+);
+
+// Get follow status and follower statistics.
+router.get(
+    "/status/:userId",
+    protect,
+    getFollowStatus
+);
+
+// Get followers of a user.
+router.get(
+    "/followers/:userId",
+    protect,
+    getFollowers
+);
+
+// Follow a user.
+router.post(
+    "/:userId",
+    protect,
+    followUser
+);
+
+// Unfollow a user.
+router.delete(
+    "/:userId",
+    protect,
+    unfollowUser
+);
 
 export default router;
